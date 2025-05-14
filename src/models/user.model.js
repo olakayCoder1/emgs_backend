b2e58b385f4b6e2af599c1f8f01c8f8038efb840
olakayCoder1 <programmerolakay@gmail.com>
2025-05-14 06:53:45 +0100

    `Update user model schema: add default value to bio field`

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema(
     role: { type: String, enum: ['user','tutor', 'admin'], default: 'user' },
     profilePicture: { type: String, default: null },
     isVerified: { type: Boolean, default: false },
-    bio: { type: String },
+    bio: { type: String, default: '' },
     preferredLanguage: { 
       type: String, 
       enum: ['English', 'German', 'Spanish', 'French', 'Dutch'], 
